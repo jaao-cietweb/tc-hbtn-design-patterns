a6e14c2c0bf9faccf06f932a1b6c3b9f1e1c2123
joaovg@ciandt.com <joaovg@ciandt.com>
2022-08-19 19:55:52 -0300

    Commit Design Patters - ex1

---
+++ 0x00/personagem-builder/Personagem.java
@@ -0,0 +1,89 @@
+abstract class Personagem {
+    String nome;
+    TipoPersonagem tipo;
+    int inteligencia;
+    int forca;
+    int vigor;
+    int resistencia;
+    int destreza;
+
+    public Personagem(String nome, TipoPersonagem tipo, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.inteligencia = inteligencia;
+        this.forca = forca;
+        this.vigor = vigor;
+        this.resistencia = resistencia;
+        this.destreza = destreza;
+    }
+
+    public abstract double getDanoAtaque();
+
+    @Override
+    public String toString() {
+    return "Personagem { nome = " + this.nome +
+            ", tipo = " + this.tipo +
+            ", inteligencia = " + this.inteligencia +
+            ", forca = " + this.forca +
+            ", vigor = " + this.vigor +
+            ", resistencia = " +  this.resistencia +
+            ", destreza = " + this.destreza +
+            ", dano ataque = "+ String.format("%.2f", getDanoAtaque()) +" }";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoPersonagem getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoPersonagem tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    public int getForca() {
+        return forca;
+    }
+
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+
+    public int getVigor() {
+        return vigor;
+    }
+
+    public void setVigor(int vigor) {
+        this.vigor = vigor;
+    }
+
+    public int getResistencia() {
+        return resistencia;
+    }
+
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+}

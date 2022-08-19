927ee96c51db6e74cb096f07f679736f40f2191e
joaovg@ciandt.com <joaovg@ciandt.com>
2022-08-19 20:09:59 -0300

    Commit Design Patters pt2 - ex1

---
+++ 0x01/cafeteria/BebidaComLeite.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class BebidaComLeite extends BebidaDecorator{
+    public BebidaComLeite(Bebida bebidaDecorada) {
+        super(bebidaDecorada);
+    }
+    @Override
+    public List<String> obterIngredientes() {
+        List<String> lista = super.obterIngredientes();
+        lista.add("leite");
+        return lista;
+    }
+
+    @Override
+    public double obterPreco() {
+        return super.obterPreco() + 3.2;
+    }
+}
---
+++ 0x01/cafeteria/BebidaDecorator.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class BebidaDecorator extends Bebida{
+    private Bebida bebidaDecorada;
+
+    public BebidaDecorator(Bebida bebidaDecorada) {
+        this.bebidaDecorada = bebidaDecorada;
+    }
+
+    @Override
+    public List<String> obterIngredientes() {
+        return this.bebidaDecorada.obterIngredientes();
+    }
+
+    @Override
+    public double obterPreco() {
+        return this.bebidaDecorada.obterPreco();
+    }
+}

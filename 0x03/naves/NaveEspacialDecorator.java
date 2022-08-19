6da3ec71a69ab9e7eb02de113142d5d2c88b553e
joaovg@ciandt.com <joaovg@ciandt.com>
2022-08-19 20:39:17 -0300

    Commit Design Patters java pt2 - ex0

---
+++ 0x03/naves/NaveEspacialDecorator.java
@@ -0,0 +1,16 @@
+public class NaveEspacialDecorator extends NaveEspacial{
+    private NaveEspacial naveDecorada;
+
+    public NaveEspacialDecorator(NaveEspacial naveEspacial) {
+        this.naveDecorada = naveEspacial;
+
+    }
+    @Override
+    public int getSaude() {
+        return this.naveDecorada.getSaude();
+    }
+    @Override
+    public int getAtaque() {
+        return this.naveDecorada.getAtaque();
+    }
+}
